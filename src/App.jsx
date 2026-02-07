ed421e0e488afaa8477573924358adade36ca216
igranic8720 <ivanmattgranic@outlook.com>
2026-02-07 03:41:41 -0500

    This is progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
 import './App.css'
+import Title from './components/Title'
+import Card, { GitHubCard } from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
   return (
     <>
-     <header>
-        WHATSUP
-     </header>
+      <div className='background'>
+        <Title/>
+        <h1>What I'm Working on</h1>
+        <div>
+          <Card>
+            <h1>LinkedIn</h1>
+          </Card>
+          <Card>
+            <h1>GitHub</h1>
+          </Card>
+          <Card>
+            <h1>Stuff</h1>
+          </Card>
+        </div>
+      </div>
+      <div>
+        <GitHubCard />
+      </div>
     </>
   )
 }
